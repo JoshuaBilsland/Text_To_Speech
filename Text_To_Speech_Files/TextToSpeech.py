7712a04554848c8325ce19eaa42ff042a4e94b03
RaxonForge <RaxonForged@protonmail.com>
2021-06-19 21:53:50 +0100

    Sppech Rate

--- Text_To_Speech_Files/TextToSpeech.py
+++ Text_To_Speech_Files/TextToSpeech.py
@@ -11,73 +11,73 @@ engine.runAndWait()
 
 # Start input loop
 continueLoop = True
-
 while continueLoop == True:
     # Change voice
     validInput = False
     while validInput == False:
-        print()
         print("Please select which voice is used")
-        print()
         maleOrFemaleVoice = input("Enter 'M' for male or 'F' for female :")
         if maleOrFemaleVoice == "M":
             voices = engine.getProperty('voices')
             engine.setProperty('voice', voices[1].id)
+            print("Voice set to male")
             engine.say("Voice set to male")
             engine.runAndWait()
             validInput = True
         elif maleOrFemaleVoice == "F":
             voices = engine.getProperty('voices')
             engine.setProperty('voice', voices[2].id)
+            print("Voice set to female")
             engine.say("Voice set to female")
             engine.runAndWait()
             validInput = True
         else:
-            print()
             print("Please only enter 'M' for male or 'F' for female")
             validInput = False
             continueLoop = True
 
+    # Change speech rate
+    validInput = False
+    while validInput == False:
+        try:    
+            rate = engine.getProperty('rate')
+            speechRate = int(input("Enter speech rate (In words per minute) :"))
+            engine.setProperty('rate', speechRate)
+            print("Speech rate set to", speechRate, "words per minute")
+            speechRateSetMessage = ("Speech rate set to",speechRate,"words per minute")
+            engine.say(speechRateSetMessage)
+            engine.runAndWait()
+            validInput = True
+        except:
+            print("Please only enter a whole number")
+            validInput = False
+
     # Convert text to speech
-    print()
     textToConvert = input("Enter text that you would like to convert to speech:")
-    print()
     engine.say(textToConvert)
     # Play the speech
     print("Playing speech...")
-    print()
     engine.runAndWait()
     # Save to mp3
     validInput = False
-
     while validInput == False:
         saveToMp3 = input("Do you want to save this speech to an mp3 file? (Y/N) :")
         if saveToMp3 == "Y":
-            print()
             print("Saving to mp3 file...")
-            print()
             engine.save_to_file(textToConvert, "TextToSpeech.mp3")
             engine.runAndWait()
-            print()
             print("Save successful")
-            print()
             validInput = True
         elif saveToMp3 == "N":
-            print()
             print("Ok, no problem")
-            print()
             validInput = True
         else:
-            print()
             print("Please only enter 'Y' or 'N'")
-            print()
-
+            
     # Asks user if they want to use the program again
     validInput = False
     while validInput == False:
-        print()
         repeatProgram = input("Do you want to use the program again? (Y/N) :")
-        print()
         if repeatProgram == "Y":
             continueLoop = True
             validInput = True
@@ -85,10 +85,8 @@ while continueLoop == True:
             continueLoop = False
             validInput = True
         else:
-            print()
             print("Please only enter 'Y' or 'N'")
-            print()
-
+            
 # End of program
 print("------------------")
 print(" Program Finished ")
